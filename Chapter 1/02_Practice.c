f6b836835c83e4e8a1f234d9a802abae15e67123
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-20 16:00:22 +0530

    Renmaed 01 | and Added practice 02 file

---
+++ Chapter 1/02_Practice.c
@@ -0,0 +1,10 @@
+// Calculate the area of circle 
+#include <stdio.h>
+int main()
+{
+    int radius = 3;
+    float pii = 3.14;
+    printf("The area of this circle is %f", pii * radius * radius);
+    return 0;
+}
+

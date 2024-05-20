e862ebd0d12c5ca2a37cbedc9b6183b0aa38d5e0
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-20 14:27:32 +0530

    Added Input.c

---
+++ Chapter 1/input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the First Number :\n ");
+    scanf("%d , &a");
+    printf("Enter The Second Number :\n2");
+    scanf("%d , &b");
+
+    printf("The Sum Of Firt & Second Number is %d", a + b);
+    return 0;
+}

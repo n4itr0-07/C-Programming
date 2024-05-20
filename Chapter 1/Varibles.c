3b58f7d18aa922cba77720e3f4445497618b57e0
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-20 13:43:26 +0530

    Added variables.c

---
+++ Chapter 1/Varibles.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a = 4;
+    // int b = 8.5; Not recommended because 8.5 is not  a integer
+    float b = 8.5;
+    char c = 'A';
+    printf("The vale of A is %d \n", a);
+    printf("The vale of b is %f \n", b);
+    printf("The vale of c is %c \n", 'A');
+
+    return 0;
+}

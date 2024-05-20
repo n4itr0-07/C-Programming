90b14ee971be66ca3fae69a12b6e9845da94d00d
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-20 13:59:29 +0530

    Modified Varibles.c

--- Chapter 1/Varibles.c
+++ Chapter 1/Varibles.c
@@ -5,9 +5,14 @@ int main()
     // int b = 8.5; Not recommended because 8.5 is not  a integer
     float b = 8.5;
     char c = 'A';
+    int d = 45;
+    int e = 45 + 10;
+
     printf("The vale of A is %d \n", a);
     printf("The vale of b is %f \n", b);
     printf("The vale of c is %c \n", 'A');
+    printf("Sum of A & D is %d \n", a + d);
+    printf("Sum of 45 + 10 is : %d \n", e);
 
     return 0;
 }

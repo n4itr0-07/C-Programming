5bee219f8f6f7754362af9c98c66b632a2d4cffb
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-20 15:52:20 +0530

    W

---
+++ Chapter 1/prt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int length, breadth;
+    printf("What is the length of reactangle \n");
+    scanf("%d", &length),
+        printf("What id the Breadth of reactangle \n");
+    scanf("%d", &breadth);
+    printf("The Area Of Your Reactangle Is : %d" , length * breadth);
+    return 0;
+}
+

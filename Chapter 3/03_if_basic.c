1c5006c803e7b6a36735e2627db6a39c56070a8d
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-25 11:16:56 +0530

    Added chapter 3 part 1

---
+++ Chapter 3/03_if_basic.c
@@ -0,0 +1,22 @@
+// TODO: C program to check whether a number is a odd or even
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter a number\n");
+    scanf("%d", &a);
+    if (a % 2 == 0)
+
+    {
+        printf("%d is an even number\n", a);
+    }
+    //FIXME: Else block is optional
+    
+    else
+    {
+        printf("%d is an odd number\n", a);
+    }
+
+    return 0;
+}

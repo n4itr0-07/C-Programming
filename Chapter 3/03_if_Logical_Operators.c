e58215f462172077f88aeca62e59a068f70bac97
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-25 11:59:23 +0530

    Logical Operators

---
+++ Chapter 3/03_if_Logical_Operators.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    int vipPass = 1;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if ((age <= 70 && age >= 18) || vipPass == 1)
+    {
+        printf("You are above 18 and below 70, can drive");
+    }
+    else{
+        printf("You cannot  drive &");
+    }
+    // if (age == 50)
+    // {
+    //     printf("Half Century");
+    // }
+    return 0;
+}
+
+// TODO: &&, || and ! are three logical operators in C. These are read as "AND", "OR",and "NOT". They are used to provide logic to our C programs.
+
+// FIXME: && --> AND --> is true when both the conditions are true.
+
+// TODO: "1 and 0" is evaluated as false.
+// TODO: "0 and 0" is evaluated as false.
+// TODO: "1 and 1" is evaluated as true.

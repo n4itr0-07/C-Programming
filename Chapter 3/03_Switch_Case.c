a5a85881965bf22c16192009da816b1b744e8fdb
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-26 07:10:13 +0530

    Switch case and Ternary Operator

---
+++ Chapter 3/03_Switch_Case.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int rating;
+    printf("Enter your rating (1-5): ");
+    scanf("%d", &rating);
+
+    switch (rating)
+    {
+    case 1:
+        printf("Your rating is 1\n");
+        break;
+     case 2:
+        printf("Your rating is 2\n");
+        break;
+    case 3:
+        printf("Your rating is 3\n");
+        break;
+    case 4:
+        printf("Your rating is 4\n");
+        break;
+    case 5:
+        printf("Your rating is 5\n");
+        break;
+    default:
+        printf("Your rating is invalid\n");
+        break;
+    }
+    return 0;
+}

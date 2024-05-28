01d4c3dc4e421e6e85daf4a51dd5049c36280db4
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-05-28 17:15:59 +0530

    Practice 5

---
+++ Chapter 3/03_Practice_6.c
@@ -0,0 +1,8 @@
+//FIXME: Write a C program that reads a student's score (an integer between 0 and 100) and determines their grade based on the following criteria:
+// A: 90-100
+// B: 80-89
+// C: 70-79
+// D: 60-69
+// F: 0-59
+
+

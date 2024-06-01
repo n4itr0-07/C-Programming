e76dcf86d900232a65206721c2ce6b7b8b31b656
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-01 08:16:08 +0530

    Chapter 6 Pointers Basic

---
+++ Chapter 6/06_Pointers_Basic.c
@@ -0,0 +1,14 @@
+v #include<stdio.h>
+
+    int main()
+{
+    int - = 34;
+    int *j = 8i; //TODO:  j will now store the address of i
+
+    printf("The value of i is %d\n", i);
+    printf("The vakue of i is %d\n", *j);
+    printf("The address of i is %u\n", &i);
+    printf("The address of i is %u\n", j); 
+    printf("The address of j is %u\n", &j); 
+    return 0;
+}

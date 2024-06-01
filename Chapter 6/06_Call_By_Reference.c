f34c196866f75fc5e955f80772870f94b81099ff
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-01 09:11:10 +0530

    Call By Reference Theory And Code

---
+++ Chapter 6/06_Call_By_Reference.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void wrong_swap(int a, int b);
+void swap(int *a, int *b);
+int main()
+{
+    int x = 3, y = 4;
+    Printf("The value of x & y before swap is %d and %d\n", x, y);
+
+    // wrong_swap(x, y); //TODO: Will not work due to call by value
+
+    swap(&x, &y); // TODO: Will work due to call by reference
+
+    printf("The value of x and y after swap is %d and %d\n", x, y);
+    return 0;
+}
+void wrong_swap(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}

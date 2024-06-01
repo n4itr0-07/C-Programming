67bf63a0c57bfa85bb660b6e55c899733878bb17
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-01 14:21:07 +0530

    Arrays Initization & pointer Arithematic

---
+++ Chapter 7/07_Arrays_Initilization.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    //TODO: for int values
+
+    int a[] = {34, 232, 23};
+    printf("The Value of a[0] is %d\n", a[0]);
+    printf("The Value of a[1] is %d\n", a[1]);
+    printf("The Value of a[2] is %d\n", a[2]);
+
+    //TODO: for float values
+    
+    float b[] = {34.23, 232.23, 23.23};
+    printf("The Value of b[0] is %f\n", b[0]);
+    printf("The Value of b[1] is %f\n", b[1]);
+    printf("The Value of b[2] is %f\n", b[2]);
+
+    return 0;
+}

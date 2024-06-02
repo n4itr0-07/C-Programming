33fe7340250f6c51a71bf231bec3caa39396dfa3
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-02 15:31:15 +0530

    Added gets & puts etc files

---
+++ Chapter 8/08_Difference_Bw_2_init_Methods.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char *ptr = "Salik Naik";
+    printf("%s\n", ptr);
+    ptr = "Salik Naik";
+    // char ptr[] =  "Salik Naik";
+    printf("%s", ptr);
+
+    return 0;
+}
---
+++ Chapter 8/08_gets_puts.c
@@ -0,0 +1,11 @@
+ #include <stdio.h> 
+ 
+ int main(){
+    char s[35];
+    printf("Enter your name :");
+    gets(s);
+    puts(s);
+   // printf("Your Name is  %s", s);
+
+    return 0;
+ }

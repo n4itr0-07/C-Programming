20813c1ef1d112d4ef82b43e127144db28839d29
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-02 15:07:01 +0530

    Added Some Example And Code

---
+++ Chapter 8/08_Convenient_Way_Print_String.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+
+int main(){
+    char *ptr = "Salik Seraj Naik";
+    printf("%s", ptr);
+    
+    return 0;
+}
---
+++ Chapter 8/08_Inputing_Strings.c
@@ -0,0 +1,10 @@
+ #include <stdio.h> 
+ 
+ int main(){
+    char s[35];
+    printf("Enter your name :");
+    scanf("%s", s);
+    printf("Your Name is  %s", s);
+
+    return 0;
+ }

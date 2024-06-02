5cc4d9aa4dd2661c87fffd102a3434b98faeca7c
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-02 09:03:37 +0530

    Added Practice Question 1

---
+++ Chapter 7/07_Practice_1.c
@@ -0,0 +1,19 @@
+//TODO: Create an array of 10 numbers. Verify using pointer arithmetic that (ptr + 2) points to the third elements where ptr is a pointer pointing to the first element of the array.
+
+#include <stdio.h> 
+
+int main(){
+    int arr[10];
+    // int *ptr = &arr[0];
+    int *ptr = arr;
+ptr = ptr+2;
+
+    if(ptr==&arr[2]){
+printf("These point to the same location in memory\n");
+
+    }
+    else{
+        printf("These do not point to the same location in memory\n");
+    }
+    return 0;
+}
---
+++ Chapter 7/07_Practice_2.c
@@ -0,0 +1,6 @@
+v #include <stdio.h> 
+
+int main(){
+    
+    return 0;
+}

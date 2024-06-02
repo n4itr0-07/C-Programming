3d6a22afd8707fc4035e4e7bc86b64acb4e98842
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-02 09:21:26 +0530

    Added Practice Question 2

--- Chapter 7/07_Practice_2.c
+++ Chapter 7/07_Practice_2.c
@@ -1,6 +1,19 @@
-v #include <stdio.h> 
+// TODO:Write a program to create an array of 10 integers and store multiplication table of 5 in it.
 
-int main(){
-    
+#include <stdio.h>
+
+int main()
+{
+    int mul[10];
+    for (int i = 0; i < 10; i++)
+    {
+        mul[i] = 30 * (i + 1);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("30X%d = %d\n", i + 1, mul[i]);
+    }
+
     return 0;
 }

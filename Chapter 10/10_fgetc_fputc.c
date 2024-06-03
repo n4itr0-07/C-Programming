7da07ee1b21754f3fc9e5bbd573082c7a77345f4
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-06-03 16:34:26 +0530

    Chapter 10 added remainig topics

---
+++ Chapter 10/10_fgetc_fputc.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *ptr;
+
+    // getc demo reading a file
+    ptr = fopen("10_getcdemo.txt", "r");
+    // char c = fgetc(ptr);
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+    printf("The vale of my character is  %c\n", fgetc(ptr));
+
+    return 0;
+}
